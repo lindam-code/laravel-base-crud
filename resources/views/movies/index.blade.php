a2203db7b25f55bfdae4a4a277e2ec8941b976b4
lindam <linda.minotti2@gmail.com>
2020-09-03 17:12:56 +0200

    Destroy.

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,8 +3,14 @@
   <ul>
     @foreach ($movies as $movie)
       <li> Titolo: {{ $movie->title }}
-        <a href={{ route('movies.show',$movie->id)}}>Vedi info film</a>
-        <a href={{ route('movies.edit',$movie->id)}}>Modifica info film</a>
+        <a href={{ route('movies.show',$movie->id) }}>Vedi info film</a>
+        <a href={{ route('movies.edit',$movie->id) }}>Modifica info film</a>
+
+        <form action="{{ route('movies.destroy',$movie->id) }}" method="post">
+          @csrf
+          @method('DELETE')
+          <input type="submit" value="Elimina">
+        </form>
       </li>
     @endforeach
   </ul>
